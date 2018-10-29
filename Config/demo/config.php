0c9e9a8c9a62f555588dd25b8a8f3022e6b63d7c
ltstudy@sina.com <ltstudy@sina.com>
2018-10-30 00:56:54 +0800

    修改端口

--- Config/demo/config.php
+++ Config/demo/config.php
@@ -27,6 +27,32 @@ $global_config_data = [
             'database' => '1'
         ]
     ],
+    'tcp' => [
+        'businessworker' => [
+            'name' => 'HskyLockBusinessWorker',
+            'count' => '8',
+            'registerAddress' => '127.0.0.1:9503'
+        ],
+        'gateway' => [
+            'name' => 'HskyLockGateway',
+            'count' => '4',
+            'lanIp' => '127.0.0.1',
+            'startPort' => '2001',
+            'protocol' => 'tcp://0.0.0.0:9503',
+            'registerAddress' => '127.0.0.1:9503',
+            'pingInterval' => 15,
+            'pingNotResponseLimit' => 4
+        ],
+        'register' => [
+            'name' => 'HskyLockRegister',
+            'protocol' => 'text://0.0.0.0:9503'
+        ],
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'database' => '1'
+        ]
+    ],
 
 //    'cabinet' => [
 //        'businessworker' => [
--- Config/tcp/config.php
+++ Config/tcp/config.php
@@ -1,5 +1,31 @@
 <?php
 $global_config_data = [
+    'demo' => [
+        'businessworker' => [
+            'name' => 'HskyLockBusinessWorker',
+            'count' => '8',
+            'registerAddress' => '127.0.0.1:9502'
+        ],
+        'gateway' => [
+            'name' => 'HskyLockGateway',
+            'count' => '4',
+            'lanIp' => '127.0.0.1',
+            'startPort' => '2000',
+            'protocol' => 'tcp://0.0.0.0:9502',
+            'registerAddress' => '127.0.0.1:9502',
+            'pingInterval' => 15,
+            'pingNotResponseLimit' => 4
+        ],
+        'register' => [
+            'name' => 'HskyLockRegister',
+            'protocol' => 'text://0.0.0.0:9502'
+        ],
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'database' => '1'
+        ]
+    ],
 
     'tcp' => [
         'businessworker' => [
@@ -27,4 +53,5 @@ $global_config_data = [
             'database' => '1'
         ]
     ],
+
 ];
