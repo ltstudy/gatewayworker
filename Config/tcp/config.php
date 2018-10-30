0c778c8c77a2b1beff20d88abf95e3cb260ca0d8
ltstudy@sina.com <ltstudy@sina.com>
2018-10-30 09:16:59 +0800

    修改

--- Config/tcp/config.php
+++ Config/tcp/config.php
@@ -1,31 +1,31 @@
 <?php
 $global_config_data = [
-//    'demo' => [
-//        'businessworker' => [
-//            'name' => 'HskyLockBusinessWorker',
-//            'count' => '8',
-//            'registerAddress' => '127.0.0.1:9502'
-//        ],
-//        'gateway' => [
-//            'name' => 'HskyLockGateway',
-//            'count' => '4',
-//            'lanIp' => '127.0.0.1',
-//            'startPort' => '12109',
-//            'protocol' => 'tcp://0.0.0.0:4000',
-//            'registerAddress' => '127.0.0.1:9502',
-//            'pingInterval' => 15,
-//            'pingNotResponseLimit' => 4
-//        ],
-//        'register' => [
-//            'name' => 'HskyLockRegister',
-//            'protocol' => 'text://0.0.0.0:9502'
-//        ],
-//        'redis' => [
-//            'host' => '127.0.0.1',
-//            'port' => '6379',
-//            'database' => '1'
-//        ]
-//    ],
+    'demo' => [
+        'businessworker' => [
+            'name' => 'HskyLockBusinessWorker',
+            'count' => '8',
+            'registerAddress' => '127.0.0.1:9502'
+        ],
+        'gateway' => [
+            'name' => 'HskyLockGateway',
+            'count' => '4',
+            'lanIp' => '127.0.0.1',
+            'startPort' => '12109',
+            'protocol' => 'tcp://0.0.0.0:4000',
+            'registerAddress' => '127.0.0.1:9502',
+            'pingInterval' => 15,
+            'pingNotResponseLimit' => 4
+        ],
+        'register' => [
+            'name' => 'HskyLockRegister',
+            'protocol' => 'text://0.0.0.0:9502'
+        ],
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'database' => '1'
+        ]
+    ],
 
     'tcp' => [
         'businessworker' => [
